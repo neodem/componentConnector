9887e8402f49b2aa6969a97d2c21d90bfab5bee1
Vincent Fumo <neodem@gmail.com>
2012-09-04 21:44:48 -0400

    adds PrettyPrint class to print a file

---
+++ src/main/java/com/neodem/componentConnector/main/PrettyPrint.java
@@ -0,0 +1,38 @@
+package com.neodem.componentConnector.main;
+
+import java.io.File;
+import java.net.URL;
+
+import com.neodem.componentConnector.io.FileConnector;
+import com.neodem.componentConnector.io.PrettyPrintOutputFileConnector;
+import com.neodem.componentConnector.model.sets.ComponentSet;
+
+/**
+ * @author vfumo
+ * 
+ */
+public class PrettyPrint {
+	
+	public PrettyPrint() {
+		ClassLoader classLoader = FullRunFromRandom.class.getClassLoader();
+		
+		URL url = classLoader.getResource("Full-Located-components.xml");
+		File componentsFile = new File(url.getPath());
+
+		url = classLoader.getResource("Full-connectables.xml");
+		File connectablesFile = new File(url.getPath());
+
+		url = classLoader.getResource("Full-connections.xml");
+		File connectionsFile = new File(url.getPath());
+
+		FileConnector c = new PrettyPrintOutputFileConnector();
+		ComponentSet set = c.read(componentsFile, connectablesFile, connectionsFile);
+
+		File out = new File("pretty.out");
+		c.writeToFile(out, set);
+	}
+
+	public static void main(String[] args) {
+		new PrettyPrint();
+	}
+}
